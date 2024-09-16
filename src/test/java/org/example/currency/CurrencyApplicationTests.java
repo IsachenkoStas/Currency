b0ceac48cab4c90082a3378ed8cd3099a9ac6293
IsachenkoStas <isachello77@gmail.com>
2024-09-16 12:02:56 +0300

    initial commit

---
+++ src/test/java/org/example/currency/CurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
